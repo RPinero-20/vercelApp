9e732fa6103451076684fc39a059f2237660bfb5
Raul Pinero <52712445+RPinero-20@users.noreply.github.com>
2023-02-11 22:07:17 -0400

    Update index.py

--- index.py
+++ index.py
@@ -1,9 +1,22 @@
 from http.server import BaseHTTPRequestHandler
 
 class handler(BaseHTTPRequestHandler):
-  def do_GET(self):
-    self.send_response(200)
-    self.send_header('Content-type','text/plain')
-    self.end_headers()
-    self.wfile.write('Hello, world!'.encode('utf-8'))
-    return
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        # self.wfile.write('Hello, world!'.encode('utf-8'))
+        with open(join('data', 'file.txt'), 'r') as file:
+            for line in file:
+            self.wfile.write(line.encode())
+        return
+
+# from http.server import BaseHTTPRequestHandler
+
+# class handler(BaseHTTPRequestHandler):
+#   def do_GET(self):
+#     self.send_response(200)
+#     self.send_header('Content-type','text/plain')
+#     self.end_headers()
+#     self.wfile.write('Hello, world!'.encode('utf-8'))
+#     return
