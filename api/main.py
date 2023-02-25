06dd7856a1dc926be3ce6646b289236de907452e
Raul Pinero <52712445+RPinero-20@users.noreply.github.com>
2023-02-24 23:12:28 -0400

    Update main.py

--- api/main.py
+++ api/main.py
@@ -4,10 +4,11 @@ from dotenv import load_dotenv
 import jwt
 
 class chatMainPoint():
-    print("Maybe yes...")
     app = Flask(__name__)
     app.register_blueprint(routes_auth, url_prefix="/api")
-    if __name__ == '__main__':
-        load_dotenv()
-        app.run(debug=True)
-        print("...I don´t know")
+    print("Maybe yes...")
+
+if __name__ == '__main__':
+    load_dotenv()
+    app.run(debug=True)
+    print("...I don´t know")
