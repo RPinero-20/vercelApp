e051533c4904f27e8a1cc553396232c9108435df
Raul Pinero <52712445+RPinero-20@users.noreply.github.com>
2023-02-24 22:18:21 -0400

    Update index.py

--- index.py
+++ index.py
@@ -1,23 +1,15 @@
 from http.server import BaseHTTPRequestHandler
 from os.path import join
+from api.main import chatMainPoint
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        # self.wfile.write('Hello, world!'.encode('utf-8'))
+        start = chatMainPoint()
+        start()
         with open(join('data', 'file.txt'), 'r') as file:
             for line in file:
                 self.wfile.write(line.encode())
         return
-
-# from http.server import BaseHTTPRequestHandler
-
-# class handler(BaseHTTPRequestHandler):
-#   def do_GET(self):
-#     self.send_response(200)
-#     self.send_header('Content-type','text/plain')
-#     self.end_headers()
-#     self.wfile.write('Hello, world!'.encode('utf-8'))
-#     return
