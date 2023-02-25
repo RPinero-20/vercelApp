92b99f655ccf9e426241570c08bfb98149083fa2
Raul Pinero <52712445+RPinero-20@users.noreply.github.com>
2023-02-24 22:03:18 -0400

    Update main.py

--- api/main.py
+++ api/main.py
@@ -3,9 +3,9 @@ from routes.auth import routes_auth
 from dotenv import load_dotenv
 import jwt
 
-def chatLogin():
-    app = Flask(__name__)
-    app.register_blueprint(routes_auth, url_prefix="/api")
-    if __name__ == '__main__':
-        load_dotenv()
-        app.run(debug=True)
+
+app = Flask(__name__)
+app.register_blueprint(routes_auth, url_prefix="/api")
+if __name__ == '__main__':
+    load_dotenv()
+    app.run(debug=True)
