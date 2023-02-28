2faf8416b267b80347f07ffd43eb62dbd381cccf
Raul Pinero <52712445+RPinero-20@users.noreply.github.com>
2023-02-28 10:47:04 -0400

    Update model_jwt.py

--- api/model_jwt.py
+++ api/model_jwt.py
@@ -1,6 +1,6 @@
+import jwt as pyjwt
 from jwt import encode, decode
 from jwt import exceptions
-import jwt as jwt
 from os import getenv
 from datetime import datetime, timedelta
 from flask import jsonify
@@ -12,13 +12,13 @@ def expire_date(days: int):
     return new_date
 
 def write_token(data: dict):
-    token = jwt.encode(payload = {**data, "exp": expire_date(2) }, key = getenv("SECRET"), algorithm = "HS256")
+    token = pyjwt.encode(payload = {**data, "exp": expire_date(2) }, key = getenv("SECRET"), algorithm = "HS256")
     return token.encode("UTF-8")
 
 def validate_token(token, output = False):
     try:
         if output:
-            return jwt.decode(token, key = getenv("SECRET"), algorithms = ["HS256"])
+            return pyjwt.decode(token, key = getenv("SECRET"), algorithms = ["HS256"])
     except exceptions.DecodeError:
         response = jsonify({"message": "Invalid Token"})
         response.status_code = 401
